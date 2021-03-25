1bb04fd7cc5284e6150f080d463deaee9bfdd17d
justenvirons <justenvirons@gmail.com>
2021-03-25 10:33:00 -0500

    Update example06_spatialjoins.R

--- scripts/example06_spatialjoins.R
+++ scripts/example06_spatialjoins.R
@@ -13,6 +13,9 @@
 ##
 ## ---------------------------
 
+# install arcgisbinding
+install.packages("arcgisbinding", repos="https://r.esri.com", type="win.binary")
+
 library(arcgisbinding)
 library(dplyr)
 library(tidyverse)
@@ -24,17 +27,35 @@ arc.check_product()
 arc.check_portal()
 arc.portal_connect("https://cookcountyil.maps.arcgis.com/")
 
+# CCDPH Target Community
+# https://cookcountyil.maps.arcgis.com/home/item.html?id=782950628c6c42a490f4b0989b07d1e0
 CC_targetcommunities_arc <- arc.open("https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/CCDPH_Target_Community/FeatureServer/0")
-CC_targetcommunities_df <- arc.select(object = CC_CCVI_arc)
-CC_targetcommunities_geom <- arc.data2sf(CC_CCVI_df)
+CC_targetcommunities_df <- arc.select(object = CC_targetcommunities_arc)
+CC_targetcommunities_geom <- arc.data2sf(CC_targetcommunities_df)
 
-CC_CCVI_arc <- arc.open("https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/CCVI_By_Muni/FeatureServer/0")
+# COVID-19 Community Vulnerability Index (CCVI) By Tract SURGO 
+# https://cookcountyil.maps.arcgis.com/home/item.html?id=8a885b47a1c7426c8cd1ac2e0fee3cbc
+CC_CCVI_arc <- arc.open("https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/ccvi_cook_tracts_2020_fc/FeatureServer/0")
 CC_CCVI_df <- arc.select(object = CC_CCVI_arc)
 CC_CCVI_geom <- arc.data2sf(CC_CCVI_df)
 
+# Social Vulnerability Index (SVI) By Census Tract CDC 
+# https://cookcountyil.maps.arcgis.com/home/item.html?id=f4cbc636c9c44b5f87183411de3157e0
+CC_SVI_arc <- arc.open("https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/sviTractsIL/FeatureServer/0")
+CC_SVI_df <- arc.select(object = CC_SVI_arc)
+CC_SVI_geom <- arc.data2sf(CC_SVI_df)
+
+# Zip Code Boundary USPS 
+# https://cookcountyil.maps.arcgis.com/home/item.html?id=66657c0939db491eb258ccc38110f3b7
 CC_ZipCodes_arc <- arc.open("https://gis12.cookcountyil.gov/arcgis/rest/services/addressZipCode/MapServer/1")
 CC_ZipCodes_df <- arc.select(object = CC_ZipCodes_arc)
 CC_ZipCodes_geom <- arc.data2sf(CC_ZipCodes_df)
+
+# Cook County Commissioner District 
+# https://cookcountyil.maps.arcgis.com/home/item.html?id=9be915367e77412cafcef725beb32a5a
+CC_CommissionerDistricts_arc <- arc.open("https://gis12.cookcountyil.gov/arcgis/rest/services/politicalBoundary/MapServer/1")
+CC_CommissionerDistricts_df <- arc.select(object = CC_CommissionerDistricts_arc)
+CC_CommissionerDistricts_geom <- arc.data2sf(CC_CommissionerDistricts_df)
 
 CC_CensusTracts_arc <- arc.open("https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/ME_COVID_Tracts/FeatureServer/0")
 CC_CensusTracts_df <- arc.select(object = CC_CensusTracts_arc)
@@ -43,10 +64,6 @@ CC_CensusTracts_geom <- arc.data2sf(CC_CensusTracts_df)
 CC_Municipalities_arc <- arc.open("https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/CCDPH_Muni_by_District/FeatureServer/0")
 CC_Municipalities_df <- arc.select(object = CC_Municipalities_arc)
 CC_Municipalities_geom <- arc.data2sf(CC_Municipalities_df)
-
-CC_CommissionerDistricts_arc <- arc.open("https://gis12.cookcountyil.gov/arcgis/rest/services/politicalBoundary/MapServer/1")
-CC_CommissionerDistricts_df <- arc.select(object = CC_CommissionerDistricts_arc)
-CC_CommissionerDistricts_geom <- arc.data2sf(CC_CommissionerDistricts_df)
 
 CC_Border_arc <- arc.open("https://gis12.cookcountyil.gov/arcgis/rest/services/plss/MapServer/1")
 CC_Border_df <- arc.select(object = CC_Border_arc)
@@ -73,9 +90,6 @@ masterlist_address_geom <- masterlist_address_geom %>% st_join(CC_CensusTracts_g
 masterlist_address_geom <- masterlist_address_geom %>% st_join(CC_Municipalities_geom_sub)
 masterlist_address_geom <- masterlist_address_geom %>% bind_cols(locations)
 masterlist_address_geom <- masterlist_address_geom %>% mutate(street = toupper(street), city=toupper(city))
-write_clip(masterlist_address_geom)
-
-write_clip(vfinder)
 
 ggplot() + geom_polygon(CC_Border_geom) + geom_point(masterlist_address_geom)
 plot(CC_CensusTracts_geom['GEOID10'])
