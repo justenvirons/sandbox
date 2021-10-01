2ffc8f9927afc237cf208c96eafee914e9476408
justenvirons <justenvirons@gmail.com>
2021-10-01 09:42:05 -0500

    added street, zone POST script for batch geocoding up to 5000 addresses

---
+++ scripts/example04b_geocoding_cookcountyservice_streetzone_POST.R
@@ -0,0 +1,98 @@
+## ---------------------------
+##
+## Script name: Geocoding in R using Cook County ArcGIS Rest API (POST request Method, using street, zone as address input)
+## Author: C. Scott Smith, PhD AICP
+## Date Created: 2021-09-17
+## Date Last Updated: 2021-10-01
+## Email: christopher.smith@cookcountyhealth.org
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+library(dplyr)
+library(tidyverse)
+library(jsonlite)
+library(httr)
+
+# Background information concerning ArcGIS REST API geocoding services
+# https://developers.arcgis.com/rest/geocode/api-reference/geocoding-geocode-addresses.htm
+# https://github.com/cengel/ArcGIS_geocoding/blob/master/SUL_gcFunctions.R #great example
+
+# Information concerning the Cook County ArcGIS REST API geocoding service
+# https://gisinternal.cookcountyil.gov/secure/rest/services/AddressLocator/CookAddressComposite/GeocodeServer?f=pjson
+
+# Download data with addresses
+# Cook County Medical Examiner data
+cc_medicalexaminercases <- 
+  read.csv(file="https://datacatalog.cookcountyil.gov/api/views/cjeq-bs86/rows.csv?accessType=DOWNLOAD")
+
+# format medical examiner data
+# street, zone format
+addresses_full <- cc_medicalexaminercases %>% 
+  select(Incident.Address, 
+         Incident.Zip.Code,
+         OBJECTID) %>%
+  drop_na(c(Incident.Address,
+            Incident.Zip.Code)) %>%
+  filter(Incident.Address!="UNK",
+         Incident.Address!="unknown",
+         Incident.Address!="", 
+         Incident.Zip.Code!="") %>%
+  mutate(Incident.Address = toupper(gsub("[^[:alnum:]]", " ",Incident.Address)),
+         Incident.Address = gsub("\\s+"," ",Incident.Address)) %>%
+  sample_n(5000) %>%
+  rename(STREET=Incident.Address, 
+         ZONE=Incident.Zip.Code) 
+
+# create input json
+addresses_json <- jsonlite::toJSON(list(records=addresses_full),flatten = T)
+addresses_text <- addresses_json %>% 
+  str_replace_all('\\{\\"STREET\\"', '\\{\\"attributes\\":\\{\"STREET\\"') %>% 
+  str_replace_all('\\},\\{\\"attributes\\"','\\}\\},\\{\\"attributes\\"') %>%
+  str_replace_all('\\}\\]\\}','\\}\\}\\]\\}')
+
+# POST request
+start_time <- Sys.time()
+geocoder_service <- "https://gisinternal.cookcountyil.gov/secure/rest/services/AddressLocator/CookAddressComposite/GeocodeServer/geocodeAddresses"
+addresses_json_rev <- rjson::fromJSON(addresses_text)
+addresses_json_rev <- jsonlite::toJSON(addresses_json_rev, flatten=TRUE, auto_unbox = TRUE)
+request_geo <- POST(url = geocoder_service,
+           body = list(addresses=addresses_json_rev,f="json"),
+           encode="form")
+result_json <- content(request_geo,"parsed","application/json")
+
+result_df <- data.frame()
+for (i in seq_len(length(result_json$locations))){
+  d <- with(result_json$locations[[i]], {data.frame(OBJECTID = attributes$ResultID,
+                                            X = as.numeric(location$x),
+                                            Y = as.numeric(location$y),
+                                            score = score, 
+                                            status = attributes$Status,
+                                            address_match = attributes$Match_addr,
+                                            side = attributes$Side,
+                                            addressType = attributes$Addr_type)})
+  result_df <- rbind(result_df, d)
+}
+
+end_time <- Sys.time()
+print(end_time - start_time)
+
+# format result dataframe, join with original addresses
+result_df <- result_df %>%
+  left_join(addresses_full, by="OBJECTID") %>%
+  rename(street_orig = STREET,
+         zone_orig = ZONE)
+
+# results summary
+result_df %>% group_by(status) %>% summarise(count=n(),
+                                             mean_score = mean(score),
+                                             min_score = min(score),
+                                             max_score = max(score),
+                                             sd_score = sd(score))
+
+# clean up
+rm(list=c('d','request_geo','result_json','addresses_full','addresses_json','addresses_json_rev','addresses_text','geocoder_service','i'))
+
